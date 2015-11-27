a514939af07952d7203bc75e46b67f02f38d2625
MrZhou <xiaojin.zhou@vipshop.com>
2015-11-27 21:08:47 +0800

    道具

---
+++ Classes/Layer/FightPropLayer.h
@@ -0,0 +1,23 @@
+//
+//  FightPropLayer.hpp
+//  HelloWorld
+//
+//  Created by MrZhou on 15/11/27.
+//
+//
+
+#ifndef __HelloWorld__FightPropLayer__
+#define __HelloWorld__FightPropLayer__
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class FightPropLayer : public CCLayer
+{
+public:
+    CREATE_FUNC(FightPropLayer);
+    virtual bool init();
+    
+};
+
+#endif
